a21c1a0c50482f6b927388f3256a883b3bd42ce4
Anton Economos <anton.economos@gmail.com>
2016-06-02 17:18:42 -0400

    Added config settings to make app embeddable

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-}
+# config.action_dispatch.default_headers = {
+#     'X-Frame-Options' => 'ALLOWALL'
+# }
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,6 +12,10 @@ Bundler.require(*Rails.groups)
 
 module Ridesharing
   class Application < Rails::Application
+
+    config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
